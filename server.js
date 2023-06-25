206e3aacfd43fd5c0c1994dd53a1bd25c9cc0162
dannabelleli <belleli.danna@gmail.com>
2023-06-25 13:10:53 -0700

    added a config and db folders. added connections, seeds, schema and server files

---
+++ server.js
@@ -0,0 +1,15 @@
+const mysql = require('mysql2');
+const inquirer = require('inquirer');
+const sequelize = require('./config/connections');
+const express = require('express');
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+sequelize.sync().then(() => {
+    app.listen(PORT, () => console.log('Now listening'));
+});
+
