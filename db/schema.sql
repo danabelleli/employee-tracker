c5a7d8197b1a0a681e919c31e0c44beaecdff062
dannabelleli <belleli.danna@gmail.com>
2023-06-25 18:07:25 -0700

    added code to the seed file and added auto increment to the tables in the schema file

--- db/schema.sql
+++ db/schema.sql
@@ -4,13 +4,13 @@ CREATE DATABASE employee_db;
 USE employee_db;
 
 CREATE TABLE department (
-  id INT NOT NULL,
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
   name VARCHAR(30) NOT NULL,
   PRIMARY KEY(id)
 );
 
 CREATE TABLE role (
-    id INT NOT NULL,
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(30) NOT NULL,
     salary DECIMAL NOT NULL,
     department_id INT,
@@ -20,7 +20,7 @@ CREATE TABLE role (
 );
 
 CREATE TABLE employess (
-    id INT NOT NULL,
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
     role_id INT NOT NULL,
