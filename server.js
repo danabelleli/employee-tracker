9b744a1034d18e5e6433f33bbf5fb41663b76050
dannabelleli <belleli.danna@gmail.com>
2023-06-26 22:15:33 -0700

    changed the connection again, started writing code in the server file. Added prompt questions and started working on the prompt question that take user input

--- server.js
+++ server.js
@@ -1,29 +1,102 @@
-const mysql = require('mysql2');
-const sequelize = require('./config/connections');
-const express = require('express');
+//const express = require('express');
+const inquirer = require('inquirer');
 
-const app = express();
-const PORT = process.env.PORT || 3001;
+const db = require('./db/connection');
+require('console.table');
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+const questions =
+    [{
+        type: 'list',
+        message: "What would you like to do?",
+        name: 'command',
+        choices: [
+            'View all departments',
+            'View all roles',
+            'View all employees',
+            'Add a department',
+            'Add a role',
+            'Add an employee',
+            'Update employee role',
+            'Quit'
+        ]
+    }];
 
-const db = mysql.createConnection(
+// hanlde add department
+const addDepartmentQuestion =
+    [
+        {
+            type: 'input',
+            messgae: 'What is the name of the department?',
+            name: 'departmentName'
+        }
+    ];
+
+const departmentAdd = (userAnswer) => {
+    db.query(`INSERT INTO department (name) VALUES(?)`, userAnswer.departmentName,
+        function (err, result) {
+            db.promise().query('select * from department').then(function (tableData) {
+                console.table(tableData[0]);
+                setTimeout(startApp, 1500);
+            });
+        });
+};
+
+const addRoleQuestion = [
     {
-        host: 'localhost',
-        // MySQL username,
-        user: 'root',
-        // MySQL password
-        password: 'password',
-        database: 'employee_db'
+        type: 'input',
+        message: 'What is the name of the role?',
+        name: 'roleName'
     },
-    console.log(`Connected to the employee_db database.`)
-);
+    {
+        type: 'input',
+        message: 'What is the salary of the role?',
+        name: 'roleSalary'
+    },
+    {
+        type: 'list',
+        message: 'Which department does the role belong to?',
+        choices: `SELECT name FROM department`
+    }
+];
 
-// example
-db.query('SELECT * FROM department', function (err, results) {
-    console.log(results);
-});
+
+
+function displayResult(option) {
+    if (option.command === 'View all departments') {
+        db.promise().query('select * from department').then(function (tableData) {
+            console.table(tableData[0]);
+            setTimeout(startApp, 1500);
+        });
+    } else if (option.command === 'View all roles') {
+        db.promise().query('select * from role').then(function (tableData) {
+            console.table(tableData[0]);
+            setTimeout(startApp, 1500);
+        });
+    } else if (option.command === 'View all employees') {
+        db.promise().query('select * from employess').then(function (tableData) {
+            console.table(tableData[0]);
+            setTimeout(startApp, 1500);
+        });
+    } else if (option.command === 'Add a department') {
+        inquirer.prompt(addDepartmentQuestion).then((userInput) => {
+            departmentAdd(userInput);
+        });
+    } else if (option.command === 'Quit') {
+        process.exit();
+    }
+}
+
+function startApp() {
+    inquirer.prompt(questions).then((option) => {
+        displayResult(option);
+    });
+}
+
+startApp();
+
+
+
+
 
 
 
