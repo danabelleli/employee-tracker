8d3ab6decae3de95e8061b1579b5c75e0fac2183
dannabelleli <belleli.danna@gmail.com>
2023-06-27 21:00:23 -0700

    added code for updating an employee role to the database

--- server.js
+++ server.js
@@ -42,7 +42,35 @@ const departmentAdd = (userAnswer) => {
         });
 };
 
-function displayResult(option) {
+async function getManagers() {
+    try {
+        const tableData = await db.promise().query(`SELECT id AS value, CONCAT(first_name,' ',last_name ) as name FROM employess WHERE manager_id is null`);
+        return tableData[0];
+
+    } catch (err) {
+        console.log(err);
+    }
+};
+
+async function getRoles() {
+    try {
+        const tableData = await db.promise().query('select id AS value, title AS name from role');
+        return tableData[0];
+    } catch (err) {
+        console.log(err);
+    }
+};
+
+async function getEmployees() {
+    try {
+        const tableData = await db.promise().query(`SELECT id AS value, CONCAT(first_name,' ',last_name ) as name FROM employess`);
+        return tableData[0];
+    } catch (err) {
+        console.log(err);
+    }
+};
+
+async function displayResult(option) {
     if (option.command === 'View all departments') {
         db.promise().query('select * from department').then(function (tableData) {
             console.table(tableData[0]);
@@ -93,6 +121,81 @@ function displayResult(option) {
         })
 
     } else if (option.command === 'Add an employee') {
+        let managers = await getManagers();
+        let roles = await getRoles();
+
+        inquirer.prompt([
+            {
+                type: 'input',
+                message: "What is the employee's first name?",
+                name: 'employeeFirstName'
+            },
+            {
+                type: 'input',
+                message: "What is the employee's last name?",
+                name: 'employeeLastName'
+            },
+            {
+                type: 'list',
+                message: 'What role belong to ?',
+                name: 'roleId',
+                choices: roles
+            },
+            {
+                type: 'list',
+                message: 'Who is the manager ?',
+                name: 'departmentId',
+                choices: managers
+            }
+        ]).then((userAnswer) => {
+            console.log(userAnswer);
+            db.query(`INSERT INTO employess (first_name, last_name, role_id, manager_id) VALUES(?,?,?,?)`, [userAnswer.employeeFirstName, userAnswer.employeeLastName, userAnswer.roleId, userAnswer.departmentId],
+                function (err, result) {
+                    if (err) {
+                        console.log(err);
+                    }
+                    else {
+                        console.log(`Added ${userAnswer.employeeFirstName} + ${userAnswer.employeeLastName} to the database`);
+                        db.promise().query('select * from employess').then(function (tableData) {
+                            console.table(tableData[0]);
+                            setTimeout(startApp, 1500);
+                        });
+                    }
+                });
+        });
+
+
+    } else if (option.command === 'Update employee role') {
+        let employees = await getEmployees();
+        let roles = await getRoles();
+        inquirer.prompt([
+            {
+                type: 'list',
+                message: "Which employee's role would you like to update?",
+                name: 'employeeID',
+                choices: employees
+            },
+            {
+                type: 'list',
+                message: "Which role would you like to assign to the selected employee?",
+                name: 'roleId',
+                choices: roles
+            }
+        ]).then((userAnswer) => {
+            db.query(`UPDATE employess SET role_id = ? WHERE id = ?`, [userAnswer.roleId, userAnswer.employeeID],
+                function (err, result) {
+                    if (err) {
+                        console.log(err);
+                    }
+                    else {
+                        console.log("Updated employee's role");
+                        db.promise().query('select * from employess').then(function (tableData) {
+                            console.table(tableData[0]);
+                            setTimeout(startApp, 1500);
+                        });
+                    }
+                });
+        });
 
     }
     else if (option.command === 'Quit') {
